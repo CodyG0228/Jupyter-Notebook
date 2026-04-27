3662b049d22b4b76dbf7d8013cf2ce0c75c33b57
Cody G. <cgunter13@ivytech.edu>
2026-04-27 17:08:56 -0400

    Yet Another Typo (YAT)

--- 15-1.py
+++ 15-1.py
@@ -1,12 +1,13 @@
 import multiprocessing
 import random
 import time
+
+
 from datetime import datetime
 
 def worker():
     sleep_time = random.random()
     time.sleep(sleep_time)
-    
     current_time = datetime.now().strftime("%H:%M:%S.%f")
     
     process_name = multiprocessing.current_process().name
@@ -14,7 +15,6 @@ def worker():
 
 if __name__ == "__main__":
     processes = []
-    
     for i in range(3):
         p = multiprocessing.Process(target=worker, name=f"Process-{i+1}")
         processes.append(p)
